357f23b19a4e64046c4f96eb3445735fc385aacd
Juan Pedro Aguiar Suarez <60158651+papainoel12310@users.noreply.github.com>
2023-07-08 22:48:45 -0300

    Create 	A - EhAb AnD gCd.cpp

---
+++ A - EhAb AnD gCd.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+ 
+using namespace std ;
+ 
+void f(){
+	int x;
+	cin >>x;
+	cout <<1<<' '<<x-1<<endl;
+}
+ 
+int main(){
+	
+	int t = 1 ;
+	cin>>t ;
+	while(t--){ 
+	    f();
+	}
+	return 0;
+}
