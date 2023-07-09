050be27fc78bb5deef1c0b66a80101f0d507e568
Juan Pedro Aguiar Suarez <60158651+papainoel12310@users.noreply.github.com>
2023-07-08 22:25:43 -0300

    Create A - A+B?.cpp

---
+++ A - A+B?.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+ 
+    
+void f(){
+    
+    int n;
+    cin>>n;
+    for(int i = 0; i<n; i++){    
+        int n1,n2;
+        char c;
+        cin>>n1;
+        cin>>c;
+        cin>>n2;
+        cout<<n1+n2<<endl;
+    }
+}    
+ 
+ 
+int main(){
+    
+ 
+    f();
+ 
+    
+}
