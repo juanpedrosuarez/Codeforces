8174f156660872f77a9e7f5270e2afc6accc6c0f
Juan Pedro Aguiar Suarez <60158651+papainoel12310@users.noreply.github.com>
2023-07-08 22:49:03 -0300

    Create A - FashionabLee.cpp

---
+++ A - FashionabLee.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main(){
+	
+	int t,n,x=0;
+	cin>>t;
+	string s;
+	while(t--){
+		cin>>n;
+		if(n%4==0) cout<<"YES"<<endl;
+		else cout<<"NO"<<endl;
+	}
+}
