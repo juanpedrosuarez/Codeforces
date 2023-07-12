5c2e1bb81cf292a4b18ac086a29f564094dfc885
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-07-12 18:32:44 -0300

    Create A - Yet Another Two Integers Problem.cpp

---
+++ A - Yet Another Two Integers Problem.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+void f(){
+    
+ 
+	int a,b,s,resp;
+	cin>>a>>b;
+    s = abs(a - b);
+    resp = ceil(s/10.0);
+    cout<<resp<<endl;
+	
+	
+}
+ 
+int main(){
+    
+	int t;
+	cin>>t;
+	while(t--){
+		f();
+	}
+ 
+}
