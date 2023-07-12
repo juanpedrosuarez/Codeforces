3bd995b142f26b00bc40773c126780c9e883915b
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-07-12 18:57:12 -0300

    Create B-ReversingEncryption.cpp

---
+++ B-ReversingEncryption.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+   
+using namespace std;
+ 
+ 
+void f() {
+ 
+  int n;
+  cin >> n;
+  string s;
+  cin >> s;
+  for(int i = 1; i <= n; i++) {
+    if(n % i == 0) {
+      reverse(s.begin(), s.begin() + i);
+    }
+  }
+  cout<<s;
+}
+ 
+int main(){
+ 
+    f();
+ 
+}
