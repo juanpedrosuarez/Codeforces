d693816e98ff80d5174bc51f454c51189e1f7a9d
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-07-12 18:38:45 -0300

    Create B - Binary String Constructing..cpp

---
+++ B - Binary String Constructing..cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+   
+using namespace std;
+ 
+ 
+void f() {
+ 
+	int a,b,x;
+	cin>>a>>b>>x;
+	int n=a+b;
+	char u,v;
+	if(a>b) u='0',v='1';
+	else u='1',v='0',swap(a,b);
+	for(int i=0; i<x/2; i++,a--,b--){
+		cout<<u<<v;
+	}
+	if(x%2==0) {
+		while(b--){
+			cout<<v;
+		}
+		while(a--){
+			cout<<u;
+		}
+	}else{
+		while(a--){
+			cout<<u;
+		}
+		while(b--){
+			cout<<v;
+		}
+	}
+}
+ 
+int main(){
+ 
+    f();
+ 
+}
