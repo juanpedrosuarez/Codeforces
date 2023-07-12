d2151c7b6cfcfb895a885090d0a45e5ebd44dcbe
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-07-12 16:34:32 -0300

    Create A - Integer Sequence Dividin.cpp

---
+++ A - Integer Sequence Dividin.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+void f(){
+	
+	int n;
+	cin>>n;
+ 
+	n=n*(n+1)/2;
+	if(n%2==0) cout<<0<<endl;
+	else cout<<1<<endl;
+ 	
+}
+ 
+int main(){
+	
+	f();
+	
+}
