e51f4bae3c89e63cce28606ee495f04fe705137a
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-07-12 18:43:25 -0300

    Create A - Two Groups.cpp

---
+++ A - Two Groups.cpp
@@ -0,0 +1,25 @@
+#include <bits\stdc++.h>
+    
+using namespace std;
+    
+void f (){
+       long long n;
+       long long s=0;
+       
+       cin>>n;
+       for(int i = 0; i<n; i++){
+         int v;
+         cin>>v;
+         s += v;
+    
+       }
+       cout<<abs(s)<<endl;
+    }
+    
+int main(){
+      int t;
+      cin>>t;
+      while(t--){
+        f();
+      }
+    }
