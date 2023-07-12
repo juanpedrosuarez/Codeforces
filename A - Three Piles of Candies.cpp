393191e130c6c2441e897b45ac104bcc0ddcd482
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-07-12 16:41:54 -0300

    Create A - Three Piles of Candies.cpp

---
+++ A - Three Piles of Candies.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+void f(){
+	
+	long long a,b,c;
+	cin>>a>>b>>c;
+	
+	long long resp = (a+b+c)/2;
+	
+	cout<<resp<<endl;
+	
+	
+}
+ 
+int main(){
+	
+	int t;
+	cin>>t;
+	while(t--){
+		f();
+	}
+	
+	
+}
