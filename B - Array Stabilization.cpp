271acaeeab01cbde08b37599496c270dbcebbc29
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-07-12 18:44:30 -0300

    Create B - Array Stabilization.cpp

---
+++ B - Array Stabilization.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+    
+using namespace std;
+    
+void f(){
+        
+        int n,resp;
+        cin>>n;
+        int v[n];
+        for(int i = 1; i <= n; i++){
+            cin>>v[i];
+        }
+        if(n == 2){
+            cout<<0<<endl;
+            return;
+        }
+        sort(v + 1, v + n + 1);
+        resp = min((v[n-1] - v[1]), v[n] - v[2]);
+        cout<<resp<<endl;
+    }
+ 
+int main(){
+        
+        f();
+        
+    }
