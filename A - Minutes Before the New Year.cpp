e30ee09a61d3f38650b1660ae5d78037c4a72fd0
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-07-12 18:30:32 -0300

    Create A - Minutes Before the New Year.cpp

---
+++ A - Minutes Before the New Year.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+void f(){
+    
+    int h,m,s=0,mt;
+	cin>>h>>m;
+	
+	mt = (24-h)*60 - m;
+	cout<<mt<<endl;
+	
+    
+}
+ 
+int main(){
+    
+	int t;
+	cin>>t;
+	while(t--){
+		f();
+	}
+ 
+}
