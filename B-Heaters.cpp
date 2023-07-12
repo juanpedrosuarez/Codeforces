44f8e2b7a66a0b7d24ce32be5686889ca5533bae
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-07-12 18:53:57 -0300

    Create B-Heaters.cpp

---
+++ B-Heaters.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+void f(){
+    
+    int n, r;
+    cin >> n >> r;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++) cin >> a[i];
+    int ans = 0;
+    int l = -1;
+    while (l < n - 1) {
+        int j = -1;
+        for (int i = n - 1; i > max((int)-1, l - r+1) and j == -1; i--) 
+            if (a[i] and i - r <= l) j = i;
+        
+        
+        if (j == -1) {
+            cout << -1 << endl;
+            return;
+        }
+        ans++;
+        l = j + r - 1;
+ 
+    }
+    cout << ans << endl;
+    
+}
+ 
+int main(){
+    
+    f();
+    
+}
