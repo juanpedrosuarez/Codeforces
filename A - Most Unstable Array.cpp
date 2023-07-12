5de04f0ba230ad0b10603ece57b3eaf490ddf20e
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-07-12 18:33:01 -0300

    Create A - Most Unstable Array.cpp

---
+++ A - Most Unstable Array.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+void f(){
+    
+ 
+	int n,m;
+	cin>>n>>m;
+	if(n<=1) cout<<0<<endl;
+	else if(n==2) cout<<m<<endl;
+	else cout<<m*2<<endl;
+	
+	
+}
+ 
+int main(){
+    
+	int t;
+	cin>>t;
+	while(t--){
+		f();
+	}
+ 
+}
