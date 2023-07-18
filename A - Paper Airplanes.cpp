450d7dda367b5173d25b32752f8ace5afddd2ff9
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-07-18 00:51:36 -0300

    Create A - Paper Airplanes.cpp

---
+++ A - Paper Airplanes.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+    int k,n,s,p,pr,ts,tp;
+ 
+    cin>> k>> n>> s>> p;
+ 
+    pr = (n-1)/s+1;
+ 
+    ts= pr * k;
+ 
+    tp = (ts-1)/p+1;
+ 
+    cout<< tp <<endl;
+ 
+ 
+ 
+ 
+}
