10d71272dd3d5854151d8fe6bca5d22c336b9148
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-07-18 00:42:44 -0300

    Create A - A oferta inacreditável.cpp

---
+++ A - A oferta inacreditável.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+ 
+int main(){
+	
+	int a,b,c,d,e;
+	cin>>a>>b>>c>>d>>e;
+	if(a==0) cout<<1;
+	if(b==0) cout<<2;
+	if(c==0) cout<<3;
+	if(d==0) cout<<4;
+	if(e==0) cout<<5;
+}
