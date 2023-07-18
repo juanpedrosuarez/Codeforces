01b3bb8204536f0b810c52299984b8e2c58ace5b
Juan Pedro Aguiar Suarez <60158651+juanpedrosuarez@users.noreply.github.com>
2023-07-18 00:44:36 -0300

    Create B-Multiplyby2divideby6.cpp

---
+++ B-Multiplyby2divideby6.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main(){
+	
+	int n,x,s,y;
+	cin>>n;
+	
+	for(int i = 0; i<n; i++){
+		s=0;
+		cin>>x;
+		while(x%6==0){
+			x = x/6;
+			s++;
+		}
+		while(x%3==0){
+			x = x/3;
+			s+=2;
+		}
+		if(x==1){ 
+			cout<<s<<endl;	 
+		}
+		else cout<<-1<<endl;
+		
+	}
+ 
+}
